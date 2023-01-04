ee22192af5a77f768447c52ded12dede35cac3e9
Kishore B <kishor.b.mca@gmail.com>
2023-01-04 12:05:08 +0530

    shell to demonistrate print

---
+++ 01-Print.sh
@@ -0,0 +1,9 @@
+echo "Print on console"
+
+echo " ************* WELCOME *********** "
+echo -e "\e[31mCOLOR TO THE TEXT\e[0m"
+echo -e "\e[32mCOLOR TO THE TEXT\e[0m"
+echo -e "\e[33mCOLOR TO THE TEXT\e[0m"
+echo -e "\e[34mCOLOR TO THE TEXT\e[0m"
+echo -e "\e[35mCOLOR TO THE TEXT\e[0m"
+echo -e "\e[36mCOLOR TO THE TEXT\e[0m"
